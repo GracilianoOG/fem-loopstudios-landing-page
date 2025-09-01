fd3707592c75cd38fe100316ab44a90b89376056
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-01 20:00:33 -0300

    Create hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import GlobalStyles from "./components/styles/GlobalStyles.styled.tsx";
+import heroImg from "./assets/images/mobile/image-hero.jpg";
+import styled from "styled-components";
+
+const Hero = styled.div`
+  background-image: url(${heroImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: min(100vh, 650px);
+  position: relative;
+`;
+
 const App = () => {
   return (
     <>
-      <div>hello world</div>
+      <GlobalStyles />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 };
