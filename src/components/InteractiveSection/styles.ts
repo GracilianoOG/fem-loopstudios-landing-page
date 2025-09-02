41e9646e20129c42e44789788b90ed628260eda0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-01 21:32:28 -0300

    Create styles of InteractiveSection

---
+++ src/components/InteractiveSection/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InteractiveSectionStyled = styled.section`
+  padding: 96px 23px 0;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export const TextBoxStyled = styled.div`
+  font-size: 21px;
+  margin-top: 48px;
+  text-align: center;
+`;
+
+export const StudioDescriptionStyled = styled.p`
+  color: var(--color-grey);
+  font-family: "Alata", sans-serif;
+  font-size: 15.3px;
+  line-height: 1.62;
+  margin-top: 16px;
+  padding: 0 16px;
+`;
