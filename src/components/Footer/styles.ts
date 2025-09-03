280e1dafd9bc649cd06ec31bfa9ae62e8fc21544
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 17:38:50 -0300

    Chance direction to column

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const FooterStyled = styled.footer`
   background-color: var(--color-black);
   display: flex;
-  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   margin-top: 95px;
   padding: 56px 16px;
 `;
