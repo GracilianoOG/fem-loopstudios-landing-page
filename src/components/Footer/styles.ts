3cad3e1aa21e0e2090830867cb1b3e8606dc8f25
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 17:42:54 -0300

    Add gap

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,6 +4,7 @@ export const FooterStyled = styled.footer`
   background-color: var(--color-black);
   display: flex;
   align-items: center;
+  gap: 30px;
   flex-direction: column;
   margin-top: 95px;
   padding: 56px 16px;
