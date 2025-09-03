25988b3cdbb705c9647d31070f7466e162030c21
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 19:19:10 -0300

    Chance image on desktop

--- src/components/CreationCard/styles.ts
+++ src/components/CreationCard/styles.ts
@@ -2,12 +2,14 @@ import styled from "styled-components";
 import type { BackgroundImageType } from "./props";
 
 export const CardStyled = styled.div<{ $bgImage: BackgroundImageType }>`
+  --bg-image-url: url(${({ $bgImage }) => $bgImage[0]});
+
   background-image: linear-gradient(
       to right,
       rgba(0, 0, 0, 0.55) 10%,
       transparent 70%
     ),
-    url(${({ $bgImage }) => $bgImage[0]});
+    var(--bg-image-url);
   background-repeat: no-repeat;
   background-size: cover;
   height: 120px;
@@ -15,6 +17,7 @@ export const CardStyled = styled.div<{ $bgImage: BackgroundImageType }>`
   width: 100%;
 
   @media screen and (min-width: 992px) {
+    --bg-image-url: url(${({ $bgImage }) => $bgImage[1]});
     height: 450px;
   }
 `;
