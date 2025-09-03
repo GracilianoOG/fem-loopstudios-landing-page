386d4952d7ec085611d985eaced526a4a16c6db0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 17:06:40 -0300

    Create Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+import headerLogo from "../../assets/images/logo.svg";
+import { LogoStyled } from "./styles";
+
+const Logo = () => {
+  return <LogoStyled src={headerLogo} alt="loopstudios" />;
+};
+
+export default Logo;
