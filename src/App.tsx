dd1823805821260220b95fb687cf0bbd4385dd8e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 16:28:31 -0300

    Import Header
    The old header content was extracted into the Header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import GlobalStyles from "./components/styles/GlobalStyles.styled.tsx";
-import headerLogo from "./assets/images/logo.svg";
-import styled from "styled-components";
 import InteractiveSection from "./components/InteractiveSection";
 import CreationSection from "./components/CreationSection";
 import Hero from "./components/Hero";
-
-const HeaderStyled = styled.header`
-  position: fixed;
-  padding: 40px 23px 0;
-  top: 0px;
-`;
-
-const LogoStyled = styled.img`
-  display: block;
-  width: 145px;
-`;
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <HeaderStyled>
-        <LogoStyled src={headerLogo} alt="" />
-      </HeaderStyled>
+      <Header />
       <main>
         <Hero />
         <InteractiveSection />
