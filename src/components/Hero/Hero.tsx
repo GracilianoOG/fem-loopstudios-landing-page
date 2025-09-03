03b5ad89875ba6e66f5ca480e302aab879f85a90
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 16:23:59 -0300

    Create Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import { HeroStyled, HeroTextStyled } from "./styles";
+
+const Hero = () => {
+  return (
+    <HeroStyled>
+      <HeroTextStyled>Immersive experiences that deliver</HeroTextStyled>
+    </HeroStyled>
+  );
+};
+
+export default Hero;
---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import heroImg from "../../assets/images/mobile/image-hero.jpg";
+
+export const HeroStyled = styled.div`
+  background-image: url(${heroImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: min(100vh, 650px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 23px;
+`;
+
+export const HeroTextStyled = styled.div`
+  border: 2px solid white;
+  color: white;
+  font-family: "Josefin Sans", sans-serif;
+  font-weight: 300;
+  font-size: 40px;
+  padding: 16px;
+  line-height: 1;
+  padding: 18px 23px;
+  text-transform: uppercase;
+`;
