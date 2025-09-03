baa39932d800a949055fa2ca988b3277cb987c40
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 17:11:54 -0300

    Create Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import Logo from "../Logo";
+import { FooterStyled } from "./styles";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <div>
+        <Logo />
+      </div>
+      <div></div>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FooterStyled = styled.footer`
+  background-color: var(--color-black);
+  display: flex;
+  justify-content: center;
+  margin-top: 95px;
+  padding: 56px 16px;
+`;
