05e479fb1c96a27c518d219b93d4da39bf6f7efd
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 16:45:38 -0300

    Created styled link

--- src/components/CreationSection/styles.ts
+++ src/components/CreationSection/styles.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const CreationSectionStyled = styled.section`
-  padding: 96px 23px 0;
+  padding: 96px 23px 75px;
+  position: relative;
 `;
 
 export const MainHeadingStyled = styled.h2`
@@ -13,4 +14,21 @@ export const MainHeadingStyled = styled.h2`
 export const CardsContainerStyled = styled.div`
   display: grid;
   gap: 24px;
+`;
+
+export const MoreLinkStyled = styled.a`
+  border: 2px solid black;
+  bottom: 0px;
+  color: black;
+  font-size: 14px;
+  font-family: "Alata", sans-serif;
+  font-weight: 300;
+  letter-spacing: 5px;
+  left: 50%;
+  margin: 0 auto;
+  padding: 10px 38px;
+  position: absolute;
+  transform: translateX(-50%);
+  text-transform: uppercase;
+  text-decoration: none;
 `;
