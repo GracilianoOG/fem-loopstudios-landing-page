75f7cfb51742905a7757568293dbaf69080b1a41
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 18:35:41 -0300

    Increase gap between footer elements

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -4,7 +4,7 @@ export const FooterStyled = styled.footer`
   background-color: var(--color-black);
   display: flex;
   align-items: center;
-  gap: 30px;
+  gap: 50px;
   flex-direction: column;
   margin-top: 95px;
   padding: 56px var(--page-gutter);
