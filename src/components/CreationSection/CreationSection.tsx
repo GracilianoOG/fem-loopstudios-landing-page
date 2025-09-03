c0002091db9db01a176b57f52d3bf3ef58772295
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 10:17:15 -0300

    Add cards

--- src/components/CreationSection/CreationSection.tsx
+++ src/components/CreationSection/CreationSection.tsx
@@ -1,9 +1,26 @@
+import styled from "styled-components";
 import { CreationSectionStyled, MainHeadingStyled } from "./styles";
+import CreationCard from "../CreationCard";
+
+export const CardsContainerStyled = styled.div`
+  display: grid;
+  gap: 24px;
+`;
 
 const CreationSection = () => {
   return (
     <CreationSectionStyled>
       <MainHeadingStyled>Our creations</MainHeadingStyled>
+      <CardsContainerStyled>
+        <CreationCard>Deep earth</CreationCard>
+        <CreationCard>Night arcade</CreationCard>
+        <CreationCard>Soccer team VR</CreationCard>
+        <CreationCard>The grid</CreationCard>
+        <CreationCard>From up above VR</CreationCard>
+        <CreationCard>Pocket borealis</CreationCard>
+        <CreationCard>The curiosity</CreationCard>
+        <CreationCard>Make it fisheye</CreationCard>
+      </CardsContainerStyled>
     </CreationSectionStyled>
   );
 };
