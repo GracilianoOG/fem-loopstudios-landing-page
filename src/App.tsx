d119a1ab197e0939bdddfae04a49aa8e71a144c4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-03 16:24:49 -0300

    Import Hero
    The hero content was extracted into the Hero component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
 import GlobalStyles from "./components/styles/GlobalStyles.styled.tsx";
-import heroImg from "./assets/images/mobile/image-hero.jpg";
 import headerLogo from "./assets/images/logo.svg";
 import styled from "styled-components";
 import InteractiveSection from "./components/InteractiveSection";
 import CreationSection from "./components/CreationSection";
-
-const Hero = styled.div`
-  background-image: url(${heroImg});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: min(100vh, 650px);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 23px;
-`;
-
-const HeroTextStyled = styled.div`
-  border: 2px solid white;
-  color: white;
-  font-family: "Josefin Sans", sans-serif;
-  font-weight: 300;
-  font-size: 40px;
-  padding: 16px;
-  line-height: 1;
-  padding: 18px 23px;
-  text-transform: uppercase;
-`;
+import Hero from "./components/Hero";
 
 const HeaderStyled = styled.header`
   position: fixed;
@@ -48,9 +24,7 @@ const App = () => {
         <LogoStyled src={headerLogo} alt="" />
       </HeaderStyled>
       <main>
-        <Hero>
-          <HeroTextStyled>Immersive experiences that deliver</HeroTextStyled>
-        </Hero>
+        <Hero />
         <InteractiveSection />
         <CreationSection />
       </main>
