358ee0acb41fb8b5251a29274eb2cd6f044dd464
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 09:04:09 -0300

    Bring header to the top layer

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,6 +4,7 @@ export const HeaderStyled = styled.header`
   position: fixed;
   padding: 40px var(--page-gutter) 0;
   top: 0px;
+  z-index: 1;
 
   @media screen and (min-width: 992px) {
     padding-top: 64px;
