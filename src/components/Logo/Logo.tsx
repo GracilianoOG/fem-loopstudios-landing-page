2610c403f9735de67203aa2aa7a24c990f6c5671
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 09:20:53 -0300

    Set small attribute to optional

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import headerLogo from "../../assets/images/logo.svg";
 import { LogoStyled } from "./styles";
 
-const Logo = ({ small }: { small: boolean }) => {
+const Logo = ({ small }: { small?: boolean }) => {
   return <LogoStyled $small={small} src={headerLogo} alt="loopstudios" />;
 };
 
