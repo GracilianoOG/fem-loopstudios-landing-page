20804f0b7c1ab5a6a6223bbc5174e5159fb04ebb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 15:58:40 -0300

    Remove header menu

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,26 +1,5 @@
-import useMobileNavMenu from "../../hooks/useMobileNavMenu";
 import NavMenuItem from "../NavMenuItem";
-import {
-  FooterListStyled,
-  HeaderListStyled,
-  HeaderNavMenuStyled,
-} from "../styles/NavStyled";
-
-export const HeaderNavMenu = () => {
-  const { isToggled } = useMobileNavMenu();
-
-  return (
-    <HeaderNavMenuStyled $hidden={!isToggled}>
-      <HeaderListStyled>
-        <NavMenuItem title="About" href="#" />
-        <NavMenuItem title="Careers" href="#" />
-        <NavMenuItem title="Events" href="#" />
-        <NavMenuItem title="Products" href="#" />
-        <NavMenuItem title="Support" href="#" />
-      </HeaderListStyled>
-    </HeaderNavMenuStyled>
-  );
-};
+import { FooterListStyled } from "../styles/NavStyled";
 
 export const FooterNavMenu = () => {
   return (
