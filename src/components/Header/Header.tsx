22775d37ea9d83e1cd01e1025f1b8389cd1e1434
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 09:35:14 -0300

    Create Hamburger component in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,19 @@ import { HamburgerStyled, HeaderStyled } from "./styles";
 import Logo from "../Logo";
 import hamburgerIcon from "../../assets/images/icon-hamburger.svg";
 
+const Hamburger = () => {
+  return (
+    <HamburgerStyled>
+      <img src={hamburgerIcon} alt="" />
+    </HamburgerStyled>
+  );
+};
+
 const Header = () => {
   return (
     <HeaderStyled>
       <Logo />
-      <HamburgerStyled>
-        <img src={hamburgerIcon} alt="" />
-      </HamburgerStyled>
+      <Hamburger />
     </HeaderStyled>
   );
 };
