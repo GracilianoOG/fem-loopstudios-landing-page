40fa37a3dfbcbbbd6ba619f3386d3a6d7b9ce493
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 15:58:54 -0300

    Remove header menu styles

--- src/components/styles/NavStyled.ts
+++ src/components/styles/NavStyled.ts
@@ -19,45 +19,6 @@ export const navListDesktopDefault = css`
   gap: 34px;
 `;
 
-export const HeaderNavMenuStyled = styled.nav<{ $hidden: boolean }>`
-  ${({ $hidden }) =>
-    $hidden &&
-    css`
-      display: none;
-    `}
-
-  @media screen and (max-width: 991px) {
-    height: 100vh;
-    inset: 0;
-    position: absolute;
-    width: 100%;
-  }
-
-  @media screen and (min-width: 992px) {
-    display: initial;
-  }
-`;
-
-export const HeaderListStyled = styled(NavListStyled)`
-  @media screen and (max-width: 991px) {
-    align-items: start;
-    background-color: black;
-    font-size: 24px;
-    font-family: "Josefin Sans", sans-serif;
-    font-weight: 300;
-    height: 100%;
-    justify-content: center;
-    padding-left: var(--page-gutter);
-    text-transform: uppercase;
-    width: 100%;
-  }
-
-  @media screen and (min-width: 992px) {
-    ${navListDefault};
-    ${navListDesktopDefault};
-  }
-`;
-
 export const FooterListStyled = styled(NavListStyled)`
   ${navListDefault};
   margin-top: 34px;
