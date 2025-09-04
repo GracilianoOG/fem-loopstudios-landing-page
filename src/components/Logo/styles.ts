e6626637f1add9871cc6a8460171b5c1a1766a90
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 14:32:11 -0300

    Set logo's z-index to 2

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 export const LogoStyled = styled.img<{ $small?: boolean }>`
   display: block;
   width: 145px;
+  z-index: 2;
 
   @media screen and (min-width: 992px) {
     ${({ $small }) =>
