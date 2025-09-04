b940f5bea5d51fb8169f26b00dfcf2543e7a0f72
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 14:33:28 -0300

    Set hamburger's z-index to 2

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -19,6 +19,7 @@ export const HamburgerStyled = styled.button`
   border: none;
   background-color: transparent;
   cursor: pointer;
+  z-index: 2;
 
   @media screen and (min-width: 992px) {
     display: none;
