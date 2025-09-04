01f18a9c9947eca2cc7f00dbbcdeeb726c5b6294
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 16:12:03 -0300

    Import new Hamburger component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,8 @@
-import { HamburgerStyled, HeaderStyled } from "./styles";
+import { HeaderStyled } from "./styles";
 import Logo from "../Logo";
-import hamburgerIcon from "../../assets/images/icon-hamburger.svg";
-import closeIcon from "../../assets/images/icon-close.svg";
 import MobileNavMenuProvider from "../../providers/MobileNavMenuProvider";
-import useMobileNavMenu from "../../hooks/useMobileNavMenu";
 import HeaderNavMenu from "../HeaderNavMenu";
-
-const Hamburger = () => {
-  const { setIsToggled, isToggled } = useMobileNavMenu();
-
-  const handleClick = () => {
-    setIsToggled(prev => !prev);
-  };
-
-  return (
-    <HamburgerStyled
-      onClick={handleClick}
-      aria-label="Toggle navigation menu"
-      aria-expanded={isToggled}
-    >
-      <img src={isToggled ? closeIcon : hamburgerIcon} alt="" />
-    </HamburgerStyled>
-  );
-};
+import Hamburger from "../Hamburger";
 
 const Header = () => {
   return (
