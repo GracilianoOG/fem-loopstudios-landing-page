804a5e1aae01e2d1c2ef049ef3d4fcfddbad48f8
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 15:55:11 -0300

    Create HeaderNavMenu component

---
+++ src/components/HeaderNavMenu/HeaderNavMenu.tsx
@@ -0,0 +1,21 @@
+import useMobileNavMenu from "../../hooks/useMobileNavMenu";
+import NavMenuItem from "../NavMenuItem";
+import { HeaderListStyled, HeaderNavMenuStyled } from "./styles";
+
+const HeaderNavMenu = () => {
+  const { isToggled } = useMobileNavMenu();
+
+  return (
+    <HeaderNavMenuStyled $hidden={!isToggled}>
+      <HeaderListStyled>
+        <NavMenuItem title="About" href="#" />
+        <NavMenuItem title="Careers" href="#" />
+        <NavMenuItem title="Events" href="#" />
+        <NavMenuItem title="Products" href="#" />
+        <NavMenuItem title="Support" href="#" />
+      </HeaderListStyled>
+    </HeaderNavMenuStyled>
+  );
+};
+
+export default HeaderNavMenu;
