9d4e1dcad6e056259af374daeb7bcd6f4208a1cb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-04 09:23:43 -0300

    Set header display to flex and center elements

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 export const HeaderStyled = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
   position: fixed;
   padding: 40px var(--page-gutter) 0;
   top: 0px;
+  width: 100%;
   z-index: 1;
 
   @media screen and (min-width: 992px) {
