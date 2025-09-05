2cde344aad9ddd4624f3a35a6a0ac66f643786e5
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-05 10:44:18 -0300

    Set media query properties as const

--- src/utils/mediaQueries.ts
+++ src/utils/mediaQueries.ts
@@ -1,6 +1,6 @@
 import { breakpoints } from "./breakpoints";
 
-const MEDIA_TYPE = "@media screen";
+const MEDIA_TYPE = "@media screen" as const;
 
 export const mediaQueries = {
   mobile: `${MEDIA_TYPE} and (min-width: ${breakpoints.sm})`,
@@ -8,4 +8,4 @@ export const mediaQueries = {
   laptop: `${MEDIA_TYPE} and (min-width: ${breakpoints.lg})`,
   desktop: `${MEDIA_TYPE} and (min-width: ${breakpoints.xl})`,
   largeDesktop: `${MEDIA_TYPE} and (min-width: ${breakpoints.xxl})`,
-};
+} as const;
