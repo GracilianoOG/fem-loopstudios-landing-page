64319b568abf82ab3fb8980260f5eed1d342155e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-06 13:33:06 -0300

    Import new Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import GlobalStyles from "./styles/GlobalStyles.styled";
-import InteractiveSection from "./components/InteractiveSection";
-import CreationSection from "./components/CreationSection";
-import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <Header />
-      <main>
-        <Hero />
-        <InteractiveSection />
-        <CreationSection />
-      </main>
+      <Main />
       <Footer />
     </>
   );
