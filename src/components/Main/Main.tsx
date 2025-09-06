ad130f1264baa049df215b7e3bacfffaf62beb93
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-06 13:32:54 -0300

    Create Main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,15 @@
+import CreationSection from "../CreationSection";
+import Hero from "../Hero";
+import InteractiveSection from "../InteractiveSection";
+
+const Main = () => {
+  return (
+    <main>
+      <Hero />
+      <InteractiveSection />
+      <CreationSection />
+    </main>
+  );
+};
+
+export default Main;
