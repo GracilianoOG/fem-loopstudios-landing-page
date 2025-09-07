6062a6e91463c7eddb98579a3352042933db6b6d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-07 08:48:04 -0300

    Convert px to rem in nav styles

--- src/styles/nav.styled.ts
+++ src/styles/nav.styled.ts
@@ -3,18 +3,18 @@ import styled, { css } from "styled-components";
 export const NavListStyled = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 1.25rem;
   list-style: none;
 `;
 
 export const navListDefault = css`
   font-family: "Alata", sans-serif;
-  font-size: 15px;
+  font-size: 0.9375rem;
   list-style: none;
   text-align: center;
 `;
 
 export const navListDesktopDefault = css`
   flex-direction: row;
-  gap: 34px;
+  gap: 2.125rem;
 `;
