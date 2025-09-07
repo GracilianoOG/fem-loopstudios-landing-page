fcda14ea8e42919a3afed0b88bcede3725ffc97a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-07 11:47:00 -0300

    Store transition time in a variable

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,13 +2,15 @@ import styled, { css } from "styled-components";
 import { mediaQueries } from "../../utils/mediaQueries";
 
 export const HeaderStyled = styled.header<{ $scrolled: boolean }>`
+  --time: 0.2s;
   align-items: center;
   display: flex;
   justify-content: space-between;
   position: fixed;
   padding: 2.5rem var(--page-gutter) 2.5rem;
   top: 0;
-  transition: background-color 0.2s, backdrop-filter 0.2s, padding 0.2s;
+  transition: background-color var(--time), backdrop-filter var(--time),
+    padding var(--time);
   width: min(100%, var(--page-max-width));
   z-index: 3;
 
