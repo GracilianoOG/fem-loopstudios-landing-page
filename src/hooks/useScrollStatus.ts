e1e650a7c5139e6bb4ba7035a0a41b558fdc0471
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-07 15:25:20 -0300

    Throttle scroll status calculation

--- src/hooks/useScrollStatus.ts
+++ src/hooks/useScrollStatus.ts
@@ -1,16 +1,27 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const useScrollStatus = (minYOffset: number) => {
   const [hasScrolled, setHasScrolled] = useState<boolean>(false);
+  const timer = useRef<number | null>(null);
+
+  const throttle = (callback: () => void, delay: number) => {
+    if (timer.current) return;
+
+    timer.current = setTimeout(() => {
+      callback();
+      timer.current = null;
+    }, delay);
+  };
 
   const handleScroll = useCallback(() => {
     setHasScrolled(window.pageYOffset >= minYOffset);
   }, [minYOffset]);
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", () => throttle(handleScroll, 200));
 
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () =>
+      window.removeEventListener("scroll", () => throttle(handleScroll, 200));
   }, [handleScroll]);
 
   return hasScrolled;
